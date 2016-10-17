6c8a9842c5d530541d52b6fc6f9a7db5473483b4
zy8082052 <1052657833@qq.com>
2016-10-17 10:59:03 +0800

    commit

---
+++ app/src/main/java/com/example/administrator/studentsystem/Video/LocalVideo.java
@@ -0,0 +1,7 @@
+package com.example.administrator.studentsystem.Video;
+
+/**
+ * Created by zhangyu on 2016-08-21.
+ */
+public class LocalVideo {
+}
---
+++ app/src/main/java/com/example/administrator/studentsystem/Video/OnlineVideo.java
@@ -0,0 +1,8 @@
+package com.example.administrator.studentsystem.Video;
+
+/**
+ * Created by zhangyu on 2016-08-21.
+ */
+public class OnlineVideo {
+
+}
